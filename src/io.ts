428dfacc6e03d711752c563a1070ddfd02475426
David Torralba Goitia <david.torralba.goitia@gmail.com>
2022-10-01 13:55:57 +0100

    Mimic Python's pathlib.Path

--- src/io.ts
+++ src/io.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as path from "path";
 
 export async function fileExists(path: string): Promise<boolean> {
   return new Promise(resolve => {
@@ -10,4 +11,55 @@ export async function fileExists(path: string): Promise<boolean> {
       }
     });
   });
+}
+
+export class Path {
+  /**
+   * Equivalent to Python's pathlib.Path
+   */
+  constructor(private readonly path: string) {}
+
+  public toString(): string {
+    return this.path;
+  }
+
+  public name(): string {
+    return path.parse(this.path).name;
+  }
+
+  public parent(): Path {
+    // TODO: remove if this is not used
+    return new Path(path.parse(this.path).dir);
+  }
+
+  public async exists(): Promise<boolean> {
+    return fileExists(this.path);
+  }
+
+  public join(child: string): Path {
+    return new Path(path.join(this.path, child));
+  }
+
+  public async walk(): Promise<Path[]> {
+    if (await this.isFile()) {
+      return [];
+    }
+
+    const children = await fs.promises.readdir(this.path);
+    return children.map(file => this.join(file));
+  }
+
+  public async isFile(): Promise<boolean> {
+    const stats = await fs.promises.lstat(this.path);
+    return stats.isFile();
+  }
+  public async isDirectory(): Promise<boolean> {
+    const stats = await fs.promises.lstat(this.path);
+    return stats.isDirectory();
+  }
+
+  public async readText(): Promise<string> {
+    // Note: EISDIR = "Error, Is Directory"
+    return fs.promises.readFile(this.path, {encoding: "utf-8"});
+  }
 }
